4671bd6576277adb2af32a8a548ae7acc618a492
Adrian Ochs <15180657+adrianochs@users.noreply.github.com>
2024-10-17 21:11:35 +0100

    still a bit to do

--- titanic/data.py
+++ titanic/data.py
@@ -20,6 +20,9 @@ def missing_data_table_analysis(data):
 
 
 def most_freq_table(data):
+    """
+    Create a frequency table.
+    """
     total = data.count()
     tt = pd.DataFrame(total)
     tt.columns = ["Total"]
@@ -37,12 +40,15 @@ def most_freq_table(data):
             vals.append(0)
             continue
     tt["Most frequent item"] = items
-    tt["Frequence"] = vals
+    tt["Frequency"] = vals
     tt["Percent from total"] = np.round(vals / total * 100, 3)
     return np.transpose(tt)
 
 
 def unique_values_table(data):
+    """
+    Create a table showing the unique values for each column.
+    """
     total = data.count()
     tt = pd.DataFrame(total)
     tt.columns = ["Total"]
@@ -52,3 +58,67 @@ def unique_values_table(data):
         uniques.append(unique)
     tt["Uniques"] = uniques
     return np.transpose(tt)
+
+
+def age_interval(data, age_col="Age"):
+    """
+    Split the age column into age intervals creating a new column called 'Age Interval'.
+    """
+    data["Age Interval"] = 0.0
+    data.loc[data["Age"] <= 16, "Age Interval"] = 0
+    data.loc[(data["Age"] > 16) & (data["Age"] <= 32), "Age Interval"] = 1
+    data.loc[(data["Age"] > 32) & (data["Age"] <= 48), "Age Interval"] = 2
+    data.loc[(data["Age"] > 48) & (data["Age"] <= 64), "Age Interval"] = 3
+    data.loc[data["Age"] > 64, "Age Interval"] = 4
+    return data
+
+
+def fare_interval(data, fare_col="Fare"):
+    """
+    Split the Fare column into fare intervals creating a new column called 'Fare Interval'.
+    """
+    data["Fare Interval"] = 0.0
+    data.loc[data["Fare"] <= 7.91, "Fare Interval"] = 0
+    data.loc[(data["Fare"] > 7.91) & (data["Fare"] <= 14.454), "Fare Interval"] = 1
+    data.loc[(data["Fare"] > 14.454) & (data["Fare"] <= 31), "Fare Interval"] = 2
+    data.loc[data["Fare"] > 31, "Fare Interval"] = 3
+    return data
+
+
+def family_size(data):
+    """
+    Create a column containing the size of the family.
+    """
+    data["Family Size"] = data["SibSp"] + data["Parch"] + 1
+    return data
+
+
+def sex_pclass(data):
+    """
+    Create a column combining the column entries from Sex and Pclass column
+    """
+    data["Sex_Pclass"] = data.apply(
+        lambda row: row["Sex"][0].upper() + "_C" + str(row["Pclass"]), axis=1
+    )
+    return data
+
+
+def parse_names(row):
+    try:
+        text = row["Name"]
+        split_text = text.split(",")
+        family_name = split_text[0]
+        next_text = split_text[1]
+        split_text = next_text.split(".")
+        title = (split_text[0] + ".").lstrip().rstrip()
+        next_text = split_text[1]
+        if "(" in next_text:
+            split_text = next_text.split("(")
+            given_name = split_text[0]
+            maiden_name = split_text[1].rstrip(")")
+            return pd.Series([family_name, title, given_name, maiden_name])
+        else:
+            given_name = next_text
+            return pd.Series([family_name, title, given_name, None])
+    except Exception as ex:
+        print(f"Exception: {ex}")
